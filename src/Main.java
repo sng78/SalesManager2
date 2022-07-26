8bee0785378b4f32230cdd75cdbfc4bc67572147
Sergei Gorbachev <sng78@list.ru>
2022-07-26 11:31:16 +0300

    Replacing int with long

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        int[] sales1 = {1, 2, 5, 586, 0, 56, -8};
+        long[] sales1 = {1, 2, 5, 586, 0, 56, -8};
         SalesManager manager1 = new SalesManager(sales1);
 
         System.out.println("Максимальная продажа - " + manager1.max());
