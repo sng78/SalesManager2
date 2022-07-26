0b058e0d5cbcc41f8ab0dabcbde3023e40a72eda
Sergei Gorbachev <sng78@list.ru>
2022-07-26 14:24:25 +0300

    Replacing int with log and add method stat

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -15,9 +15,9 @@ public class SalesManager {
         return max;
     }
 
-    public int min() {
-        int min = -1;
-        for (int sale : sales) {
+    public long min() {
+        long min = -1;
+        for (long sale : sales) {
             if (sale < min) {
                 min = sale;
             }
@@ -25,16 +25,15 @@ public class SalesManager {
         return min;
     }
 
-    public int stat() {
-        int sum = 0;
+    public long stat() {
+        long sum = 0;
         int count = 0;
-        for (int sale : sales) {
+        for (long sale : sales) {
             if (sale != max() && sale != min()) {
                 sum += sale;
                 count += 1;
             }
         }
-        int stat = sum / count;
-        return stat;
+        return sum / count;
     }
 }
